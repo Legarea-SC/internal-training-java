7aad265989c3f19366811825f7cb85f32ddd0085
篠島吉貴 <sasajimayoshitaka@gmail.com>
2026-07-07 15:50:02 +0900

    add:必要なファイルの追加

--- src/main/java/com/example/LEGAREA/entity/StaffDetailEntity.java
+++ src/main/java/com/example/LEGAREA/entity/StaffDetailEntity.java
@@ -1,12 +1,14 @@
 package com.example.LEGAREA.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class StaffDetailEntity {
-    private String staffid;
+    private String staffId;
     private String name;
     private String division;
     private String firstName;
--- src/main/java/com/example/LEGAREA/entity/StaffInputEntity.java
+++ src/main/java/com/example/LEGAREA/entity/StaffInputEntity.java
@@ -1,4 +1,33 @@
 package com.example.LEGAREA.entity;
 
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StaffInputEntity {
+
+    @NotBlank(message = "社員コードは必須入力です")
+    private String staffId;
+    @NotBlank(message = "名前は必須入力です")
+    private String name;
+
+    private String division; // 任意
+
+    @NotBlank(message = "セイは必須入力です")
+    private String firstName;
+
+    @NotBlank(message = "メイは必須入力です")
+    private String lastName;
+
+    private String position; // 任意
+
+   @NotNull(message = "年齢は必須入力です")
+    private Integer age;
 }
